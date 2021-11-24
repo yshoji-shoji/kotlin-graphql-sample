782aff12dab1f00ba0343648baafbccd37127c42
yshoji-shoji <43976074+yshoji-shoji@users.noreply.github.com>
2021-11-24 14:39:56 +0900

    プロジェクトの雛形を作成

---
+++ src/main/kotlin/com/example/kotlingraphqlsample/KotlinGraphqlSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlingraphqlsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinGraphqlSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinGraphqlSampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlingraphqlsample/KotlinGraphqlSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlingraphqlsample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinGraphqlSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
