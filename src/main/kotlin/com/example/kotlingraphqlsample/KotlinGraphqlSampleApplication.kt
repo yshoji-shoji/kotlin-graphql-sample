f1b12f8795a0629644f0eec9413cedd537df77b6
yshoji-shoji <43976074+yshoji-shoji@users.noreply.github.com>
2021-11-25 11:28:35 +0900

    detektのチェックに引っかかっていた箇所を無視するように

--- src/main/kotlin/com/example/kotlingraphqlsample/KotlinGraphqlSampleApplication.kt
+++ src/main/kotlin/com/example/kotlingraphqlsample/KotlinGraphqlSampleApplication.kt
@@ -7,5 +7,6 @@ import org.springframework.boot.runApplication
 class KotlinGraphqlSampleApplication
 
 fun main(args: Array<String>) {
+    @Suppress("SpreadOperator")
     runApplication<KotlinGraphqlSampleApplication>(*args)
 }
